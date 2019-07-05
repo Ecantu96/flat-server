19fc7cd0ff90749d2d972de8659b83737e5220d7
Jeff Chiu <jeffchiu2000@gmail.com>
2019-07-04 18:19:01 -0700

    update the config and db

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb+srv://nooklyn-flats-backend-apis:gNZSs83j7E2eAH3C@cluster0-6mbxf.mongodb.net/test?retryWrites=true&w=majority'
+    url: 'mongodb+srv://nooklyn-flats-backend-apis:zWHJ7EJylE1BWc0k@cluster0-6mbxf.mongodb.net/test?retryWrites=true&w=majority'
 }
 
    
